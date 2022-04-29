790383e6d04ba02e4c523d5ce562664d87812c91
vicenteyah <vicente.yah.dzul@icloud.com>
2022-04-29 16:43:00 -0500

    timestamps added to de notes model

--- notes/models.py
+++ notes/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class Note(models.Model):
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
