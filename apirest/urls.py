a74e7170dd43d5aa6a20332e6e008690b89b3e03
vicenteyah <vicente.yah.dzul@icloud.com>
2022-04-29 16:41:28 -0500

    creating a schemaview to use swagger library

--- apirest/urls.py
+++ apirest/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from django.urls import path , include
-from rest_framework import routers
+from django.urls import path , include 
+from rest_framework import routers, permissions
 from notes.views import NoteViewSet
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Notes API rest",
+        default_version='v1',
+        description="Test description",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 notes_router = routers.SimpleRouter()
 notes_router.register(
@@ -13,7 +28,7 @@ notes_router.register(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path('api/', include(notes_router.urls)),
 ]
 
