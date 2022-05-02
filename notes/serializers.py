638cf607247ebf164631d17c983f1fa4f1e19ca8
vicenteyah <vicente.yah.dzul@icloud.com>
2022-05-02 18:09:15 -0500

    expilicit response json in serializer, adding kwargs too

--- notes/serializers.py
+++ notes/serializers.py
@@ -4,4 +4,15 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ('__all__')
+        fields = (
+            'id', 
+            'title', 
+            'comment', 
+            'created_at', 
+            'updated_at'
+        )
+        extra_kwargs = {
+            "title": {"required": False, "allow_blank": True},
+            "comment": {"required": False, "allow_blank": True},
+        }
+
