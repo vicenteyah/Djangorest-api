743b982bbabe09a613dfa44338eef9ce45b40e47
vicenteyah <vicente.yah.dzul@icloud.com>
2022-05-02 18:04:32 -0500

    json web token library installed

--- apirest/settings.py
+++ apirest/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     # 3rd party apps\
     'notes',
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': env('PAGINATION_PAGE_SIZE'),
+}
