f8e5674864291a2bb09cd6182226bfc4bc5b406a
vicenteyah <vicente.yah.dzul@icloud.com>
2022-05-02 18:08:12 -0500

    model was modified adding new attribute

--- notes/models.py
+++ notes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    text = models.TextField()
+    title = models.CharField(max_length=200)
+    comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
